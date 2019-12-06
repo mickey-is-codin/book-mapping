2ea7777e56a43eb222ed9c3c202b09d5d90060ad
Mickey Smith <smithmick14@gmail.com>
2019-12-06 15:44:28 -0600

    Trying to work on routing

--- book-garden/src/components/Home.js
+++ book-garden/src/components/Home.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../index.css';
 
-class Home extends React.Component {
+import Particles from 'react-particles-js';
+import particleOpts from '../particles/particle-options.js';
 
-    render() {
-        return(
-            <div>
-                <h1> Home </h1>
-            </div>
-        );
-    }
+function Home() {
+  return (
+    <div>
+      <Background/>
+    </div>
+  );
+}
+
+function Background() {
+  return (
+    <div className="particles-container">
+      <Particles className="particles" params={particleOpts}/>
+    </div>
+  );
 }
 
 export default Home;
--- book-garden/src/index.js
+++ book-garden/src/index.js
@@ -3,5 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+
+
+import Home from './components/Home';
+import Build from './components/Build';
+import Login from './components/Login';
+import Register from './components/Register';
+
+const routing = (
+    <Router>
+        <Route path="/" component={Home} />
+        <Route path="/build" component={Build} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
